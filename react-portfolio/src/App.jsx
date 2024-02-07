517da1a79830bf33ddcade0a45145a3577917e37
Nasiphi Ngcobozi <nasiphigee@gmail.com>
2024-02-07 15:05:46 +0200

    Implemented the navigation page

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+
+import { Navigation } from './components/navigation/navbar.jsx';
+import { Home } from './components/home/home.jsx';
+import './App.css'
+
+function App() {
+  return (
+    <Navigation />
+  )
+}
+
+export default App
---
+++ react-portfolio/src/components/navigation/navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./navbar.css";
+
+export const Navigation = () => {
+    return (
+        <div>
+            <div className="group-5"></div>
+            <div className="text-wrapper-43">Contact</div>
+            <div className="text-wrapper-44">Project</div>
+            <div className="text-wrapper-45">Skills</div>
+            <div className="text-wrapper-46">About Me</div>
+            <div className="text-wrapper-47">Home</div>
+            <img
+                className="desktop-wallpaper"
+                alt="Desktop wallpaper"
+                src="https://cdn.animaapp.com/projects/65c334fcf4d3256a5645b298/releases/65c33566a111d9ebe1d3014f/img/desktop-wallpaper-infinity-symbol-cool-symbols-1.png"
+            />
+        </div>
+    );
+}
+
+export default Navigation 
