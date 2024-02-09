2dcf6a4fdcd8fc54f25d0fc6778d7b6e8e11250f
Nasiphi Ngcobozi <nasiphigee@gmail.com>
2024-02-09 13:21:49 +0200

    working on Education and Experience

--- react-portfolio/src/components/about/about.jsx
+++ react-portfolio/src/components/about/about.jsx
@@ -1,116 +1,64 @@
 import React from "react";
+/* Example: src/styles/styles.css */
+import '~@fortawesome/fontawesome-free/css/all.css';
+import { facalendar } from '@fortawesome/free-solid-svg-icons';
 import "./about.css";
 export const About = () => {
   return (
-    <div className="about">
-      <div className="text-wrapper-6">ABOUT ME</div>
-      <div className="overlap-2">
-        <div className="rectangle-4" />
-        <img
-          className="img"
-          alt="about-me-image"
-          src="./src/assets/images/AboutMe.png"
-        />
-      </div>
-      <div className="text-wrapper-7">I’m Nasiphi Gwija</div>
-      <p className="full-stack-dev">
-        <span className="text-wrapper-8">
-          Full Stack Dev Learnership @ Uvu Africa <br />
-        </span>
-        <span className="text-wrapper-9">
-          Specializing in web and software development. Proficient in front and back-end technologies, Explore my
-          portfolio to my diverse range of projects &amp; expertise.
-        </span>
-      </p>
-      <div className="rectangle-5" />
-      <div className="text-wrapper-10">2012-2012</div>
-      <div className="text-wrapper-11">Matric</div>
-      <p className="i-m-a-software">
-        {" "}
-        I&#39;m a software developer with a passion for crafting digital experiences that are not only functional
-        but also delightful.
-        <br />
-        My coding journey began with the excitement of creating my first &#39;Hello World&#39; program, and it has
-        since evolved into a love affair with both front-end and back-end technologies.
-        <br />
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;EDUCATION&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        EXPERIENCE
-      </p>
-      <div className="text-wrapper-12">2023-2024</div>
-      <div className="text-wrapper-13">2014-2017</div>
-      <img
-        className="bx-calendar-svg"
-        alt="Bx calendar svg"
-        src="https://cdn.animaapp.com/projects/65c334fcf4d3256a5645b298/releases/65c33566a111d9ebe1d3014f/img/bx-calendar-svg.svg"
-      />
-      <p className="bsc-in-IT-richfield">
-        <span className="text-wrapper-14">
-          BSc in IT @ Richfield Institute of Technology
-          <br />
-        </span>
-        <span className="text-wrapper-15">My academic journey has equipped me with a strong foundation in IT</span>
-        <span className="text-wrapper-16">&nbsp;</span>
-        <span className="text-wrapper-15"> principles and a deep expertise in</span>
-        <span className="text-wrapper-16">&nbsp;</span>
-        <span className="text-wrapper-15">Software Dev. </span>
-      </p>
-      <div className="overlap-3">
-        <img
-          className="line"
-          alt="Line"
-          src="https://cdn.animaapp.com/projects/65c334fcf4d3256a5645b298/releases/65c33566a111d9ebe1d3014f/img/line-1.svg"
-        />
-        <div className="ellipse" />
-      </div>
-      <div className="ellipse-2" />
-      <img
-        className="bx-calendar-svg-2"
-        alt="Bx calendar svg"
-        src="https://cdn.animaapp.com/projects/65c334fcf4d3256a5645b298/releases/65c33566a111d9ebe1d3014f/img/bx-calendar-svg.svg"
-      />
-      <img
-        className="bx-calendar-svg-3"
-        alt="Bx calendar svg"
-        src="https://cdn.animaapp.com/projects/65c334fcf4d3256a5645b298/releases/65c33566a111d9ebe1d3014f/img/bx-calendar-svg.svg"
-      />
-      <img
-        className="line-2"
-        alt="Line"
-        src="https://cdn.animaapp.com/projects/65c334fcf4d3256a5645b298/releases/65c33566a111d9ebe1d3014f/img/line-2.svg"
-      />
-      <img
-        className="ellipse-3"
-        alt="Ellipse"
-        src="https://cdn.animaapp.com/projects/65c334fcf4d3256a5645b298/releases/65c33566a111d9ebe1d3014f/img/ellipse-3.svg"
-      />
-      <img
-        className="bx-calendar-svg-4"
-        alt="Bx calendar svg"
-        src="https://cdn.animaapp.com/projects/65c334fcf4d3256a5645b298/releases/65c33566a111d9ebe1d3014f/img/bx-calendar-svg.svg"
-      />
-      <div className="text-wrapper-17">2024- Present</div>
-      <img
-        className="line-3"
-        alt="Line"
-        src="https://cdn.animaapp.com/projects/65c334fcf4d3256a5645b298/releases/65c33566a111d9ebe1d3014f/img/line-3.svg"
-      />
-      <p className="text-wrapper-18">Junior Software Dev @ Absa (Younglings)</p>
-      <p className="this-educational">
-        This educational background
-        <br />
-        reflects my strong foundation in quantitative reasoning, scientific principles,
-        <br />
-        and geographical knowledge. It has not only instilled problem-solving
-        <br />
-        skills but also a deep understanding of the natural world and our environment.
-      </p>
-      <p className="in-my-current-role-i">
-        In my current role, I am part of a dynamic team where I contribute in developing web applications, working
-        on APIs, I am particularly passionate about frontend development and I&#39;m actively seeking opportunities
-        to grow in this area.
-      </p>
-    </div>
+    <div class="row">
+        <div class="col-lg-6">
+          <h3 class="resume-title">About Me</h3>
+          <div class="resume-item pb-0">
+            <h4>I'm Nasiphi Gwija</h4>
+            <p><em> I'm a software developer with a passion for crafting digital experiences that are not only functional but also delightful.
+                    My coding journey began with the excitement of creating my first 'Hello World' program, and it has since evolved into a love
+                     affair with both front-end and back-end technologies.</em></p>
+          <h3 class="resume-title">Education</h3>
+          <i class="fa-solid fa-calendar-days"></i>
+          <h5>2014-2017</h5>
+          <div class="resume-item">
+            <h4>BSc in IT @ Richield Graduate Institute of technology</h4>
+            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+            <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+          </div>
+          <div class="resume-item">
+            <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
+            <h5>2010 - 2014</h5>
+            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+            <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <h3 class="resume-title">Professional Experience</h3>
+          <div class="resume-item">
+            <h4>Senior graphic design specialist</h4>
+            <h5>2019 - Present</h5>
+            <p><em>Experion, New York, NY </em></p>
+            <p>
+            <ul>
+              <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
+              <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
+              <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
+              <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+            </ul>
+            </p>
+          </div>
+          <div class="resume-item">
+            <h4>Graphic design specialist</h4>
+            <h5>2017 - 2018</h5>
+            <p><em>Stepping Stone Advertising, New York, NY</em></p>
+            <p>
+            <ul>
+              <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+              <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+              <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+              <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+            </ul>
+            </p>
+          </div>
+        </div>
+        </div>
+        </div>
   );
 };
 
