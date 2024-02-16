1722ba4d3bddc96e174abbc557cea899c601a648
Nasiphi Ngcobozi <nasiphigee@gmail.com>
2024-02-16 15:16:46 +0200

    Worked on Projects page

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,6 +3,8 @@ import Navigation from './components/navigation/navbar.jsx';
 import { Home } from './components/home/home.jsx';
 import About from './components/about/about.jsx';
 import Skills from './components/skills/skills.jsx';
+import Projects from './components/Projects/project.jsx';
+import Contact from './components/contact/contact.jsx';
 
 
 
@@ -11,6 +13,8 @@ function App() {
     <><Navigation />
       <Home />  <About />
       <Skills />
+      <Projects />
+      <Contact />
     </>
 
   )
