dccc8d0a039e2f32726702d4e4bb77cc6878578f
nasiphigee <nasiphigee@gmail.co>
2024-02-22 17:28:43 +0200

    Worked on Skills

--- react-portfolio/src/components/skills/skills.jsx
+++ react-portfolio/src/components/skills/skills.jsx
@@ -1,82 +1,106 @@
-// skills.jsx
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'ms-react-progress-bar/dist/ProgressBar.css';
 import './skills.css';
-import { Waypoint } from 'react-waypoint';
-
 
 function Skills() {
-  
+  const [skillsEntered, setSkillsEntered] = useState(false);
+
   const handleWaypointEnter = () => {
-    let progress = document.querySelectorAll('.progress-bar');
-    progress.forEach((el) => {
-      el.style.width = el.getAttribute('aria-valuenow') + '%';
-    });
+    if (!skillsEntered) {
+      let progress = document.querySelectorAll('.progress-bar');
+      progress.forEach((el) => {
+        el.style.width = el.getAttribute('aria-valuenow') + '%';
+      });
+      setSkillsEntered(true);
+    }
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const skillsSection = document.getElementById('skills');
+      const rect = skillsSection.getBoundingClientRect();
+
+      if (rect.top <= window.innerHeight && !skillsEntered) {
+        handleWaypointEnter();
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [skillsEntered]);
+
   return (
-    <section id="skills" class="skills section-bg">
-      <div class="container">
-
-        <div class="section-title">
+    <section id="skills" className="skills">
+      <div className="container" data-aos="fade-up">
+        <div className="section-title">
           <h2>Skills</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
-
-        <div class="row skills-content">
-
-          <div class="col-lg-6" data-aos="fade-up">
-
-            <div class="progress">
-              <span class="skill">HTML <i class="val">100%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+        <div className="row skills-content">
+          <div className="col-lg-6">
+            <h3>Front-End</h3>
+            <div className="progress">
+              <span className="skill">HTML <i className="val">100%</i></span>
+              <div className="progress-bar-wrap">
+                <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
 
-            <div class="progress">
-              <span class="skill">CSS <i class="val">90%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+            <div className="progress">
+              <span className="skill">CSS <i className="val">95%</i></span>
+              <div className="progress-bar-wrap">
+                <div className="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
 
-            <div class="progress">
-              <span class="skill">JavaScript <i class="val">75%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            <div className="progress">
+              <span className="skill">JavaScript <i className="val">85%</i></span>
+              <div className="progress-bar-wrap">
+                <div className="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
 
+            <div className="progress">
+              <span className="skill">PHP<i className="val">80%</i></span>
+              <div className="progress-bar-wrap">
+                <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
           </div>
 
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-
-            <div class="progress">
-              <span class="skill">PHP <i class="val">80%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="col-lg-6">
+            <h3>Back-End</h3>
+            <div className="progress">
+              <span className="skill">Java<i className="val">85%</i></span>
+              <div className="progress-bar-wrap">
+                <div className="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
 
-            <div class="progress">
-              <span class="skill">WordPress/CMS <i class="val">90%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+            <div className="progress">
+              <span className="skill">React<i className="val">70%</i></span>
+              <div className="progress-bar-wrap">
+                <div className="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
 
-            <div class="progress">
-              <span class="skill">Photoshop <i class="val">55%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+            <div className="progress">
+              <span className="skill">Node.js<i className="val">70%</i></span>
+              <div className="progress-bar-wrap">
+                <div className="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div className="progress">
+              <span className="skill">C#<i className="val">65%</i></span>
+              <div className="progress-bar-wrap">
+                <div className="progress-bar" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <Waypoint onEnter={handleWaypointEnter} bottomOffset="80%" />
     </section>
   );
 }
